0ca54b8e35e4fa9d160a1dbde58fc45c59727724
rwu780 <rwu4@ualberta.ca>
2022-07-09 22:08:35 -0700

    complete first draft for dashboard UI

--- app/src/main/java/com/rwu780/weatherapp/MainActivity.kt
+++ app/src/main/java/com/rwu780/weatherapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.rwu780.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.rwu780.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
     }
+
 }
---
+++ app/src/main/java/com/rwu780/weatherapp/ui/DashboardFragment.kt
@@ -0,0 +1,38 @@
+package com.rwu780.weatherapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rwu780.weatherapp.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var _binding : FragmentDashboardBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDashboardBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+    private fun initView() {
+//        TODO("Not yet implemented")
+    }
+
+
+}
