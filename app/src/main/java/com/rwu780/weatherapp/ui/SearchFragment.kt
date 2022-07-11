b051d1a41cf3533bac97d9e46c3f7d0aeb4d38c8
rwu780 <rwu4@ualberta.ca>
2022-07-11 13:41:10 -0700

    Add search fragment

--- app/src/main/java/com/rwu780/weatherapp/ui/SearchFragment.kt
+++ app/src/main/java/com/rwu780/weatherapp/ui/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.rwu780.weatherapp.ui
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,47 +8,37 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.rwu780.weatherapp.R
-import com.rwu780.weatherapp.databinding.FragmentDashboardBinding
+import com.rwu780.weatherapp.databinding.FragmentSearchBinding
 
-class DashboardFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private lateinit var _binding : FragmentDashboardBinding
+    private lateinit var _binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentDashboardBinding.inflate(
-            inflater,
-            container,
-            false
-        )
+
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        (activity as AppCompatActivity).supportActionBar?.hide()
         return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initView()
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        _binding.appBar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
     override fun onResume() {
         (activity as AppCompatActivity).supportActionBar?.hide()
         super.onResume()
     }
-
-    private fun initView() {
-        _binding.iconSetting.setOnClickListener {
-            findNavController().navigate(R.id.action_dashboardFragment_to_settingsFragment)
-        }
-
-        _binding.iconSearch.setOnClickListener {
-            findNavController().navigate(R.id.action_dashboardFragment_to_searchFragment)
-        }
-
-    }
-
-
 }
--- app/src/main/java/com/rwu780/weatherapp/ui/SettingsFragment.kt
+++ app/src/main/java/com/rwu780/weatherapp/ui/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.rwu780.weatherapp.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import com.rwu780.weatherapp.R
 
@@ -8,6 +9,16 @@ import com.rwu780.weatherapp.R
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference, rootKey)
+    }
+
+    override fun onResume() {
+        (activity as AppCompatActivity).supportActionBar?.show()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        super.onPause()
     }
 
 }
